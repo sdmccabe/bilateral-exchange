cefa50a856ac384f295a952d39734883bf68c475
Stefan McCabe <stefanmccabe@gmail.com>
2016-02-25 18:11:34 -0500

    batch run stuff - mostly for easy access on Big Box

---
+++ serial-activation-suite/batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#This script runs the parameterizations necessary to generate an initial benchmark of the model.
+cd ..
+make
+cd serial-activation-suite
+
+echo "agents,real,user,sys,memory" > ./activation-benchmark.csv
+
+declare -a parameters=(-1 0 1 2 3 4)
+for x in ${parameters[@]}
+do
+	for ((i=1; i<25; i++))
+	do
+		#note, using gtime, replace with /usr/bin/time on linux
+		{ /usr/bin/time -f "${x},%e,%U,%S,%M" ../exchange -file=parameters-${x}.cfg ; } 2>> ./activation-benchmark.csv
+	done
+
+done
