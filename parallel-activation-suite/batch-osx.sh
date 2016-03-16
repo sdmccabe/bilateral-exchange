2572fbd55fc97cba0d5488853f80cdf911b6ced3
Stefan McCabe <stefanmccabe@gmail.com>
2016-03-16 19:01:25 -0400

    Added missing batch files

---
+++ parallel-activation-suite/batch-osx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#This script runs the parameterizations necessary to generate an initial benchmark of the model.
+cd ..
+make
+cd parallel-activation-suite
+
+echo "agents,real,user,sys,memory" > ./activation-benchmark.csv
+
+declare -a parameters=(-1 0 1 2 3 4 5)
+for x in ${parameters[@]}
+do
+	for ((i=0; i<5; i++))
+	do
+		#note, using gtime, replace with /usr/bin/time on linux
+		{ gtime -f "${x},%e,%U,%S,%M" ../exchange -file=parameters-${x}.cfg ; } 2>> ./activation-benchmark.csv
+	done
+
+done
